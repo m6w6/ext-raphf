e2b5953d34381c54d4efff501124afe24ec057b0
Michael Wallner <mike@php.net>
2019-06-17 13:49:42 +0200

    travis: explicitly test against 7.4

--- scripts/gen_travis_yml.php
+++ scripts/gen_travis_yml.php
@@ -16,7 +16,7 @@ env:
 $cur = "7.3";
 $gen = include "./travis/pecl/gen-matrix.php";
 $env = $gen([
-	"PHP" => ["7.0", "7.1", "7.2", "master"],
+	"PHP" => ["7.0", "7.1", "7.2", "7.4", "master"],
 	"enable_debug" => "yes",
 	"enable_maintainer_zts" => "yes",
 ], [
@@ -37,7 +37,7 @@ matrix:
  allow_failures:
 <?php
 $allow_failures = array_merge( ... array_map(function($a) {
-	return preg_grep('/^PHP=master /', $a);
+	return preg_grep('/^PHP=(master|7.4) /', $a);
 }, $env));
 foreach ($allow_failures as $e) {
 	printf("  - env: %s\n", $e);
